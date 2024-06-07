4186099d244dbbf3af0064c34c29310c0b5e3e37
JamesGwapogi <167041980+JamesGwapogi@users.noreply.github.com>
2024-06-07 16:13:27 +0800

    sorry po

---
+++ CashierApplicationn/Class3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApplication
+{
+    internal abstract class UserAccount
+    {
+        protected string full_name;
+        protected string user_name;
+        protected string user_password;
+
+        public UserAccount(string name, string uName, string password)
+        {
+            this.full_name = "James Carrascal";
+            this.user_name = "cashier1234";
+            this.user_password = "qwerty123";
+        }
+
+        public abstract bool validateLogin(string uName, string password);
+        public string getFullName()
+        {
+            return this.full_name;
+        }
+    }
+}
--- CashierApplicationn/DiscountedItem.cs
+++ CashierApplicationn/DiscountedItem.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace CashierApplicationn
 {
-    public class DiscountedItem_ : Item
-    { 
-
+    public class DiscountedItem : Item
+    {
         private double item_discount;
         private double discounted_price;
         private double payment_amount;
         private double change;
 
-        public DiscountedItem_(string name, double price, int quantity, double discount) : base (name, price, quantity) {
-            this.item_discount = discount = discount * 0.01;
-            this.discounted_price = 0.0;
-            this.payment_amount = 0.0;
-            this.total_price = 0.0;
 
-        
+        public DiscountedItem(string name, double price, int quantity, double discount)
+          : base(name, price, quantity)
+        {
+            this.item_discount = discount * 0.01;
+            this.discounted_price = 00.00;
+            this.payment_amount = 00.00;
+            this.change = 00.00;
         }
         public override double getTotalPrice()
         {
@@ -28,15 +28,18 @@ namespace CashierApplicationn
             total_price = discounted_price * item_quantity;
             return total_price;
         }
+
         public override void setPayment(double amount)
         {
             this.payment_amount = amount;
         }
-        public double getChange() {
-            change =  payment_amount - total_price;
+
+        public double getChange()
+        {
+            change = payment_amount - total_price;
             return change;
         }
     }
-    
+
 }
 
